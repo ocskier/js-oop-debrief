b6828d9dd1feab6751bb8bf9f97a2d4cc4e1c234
Martin Renteria <renteriamp96@gmail.com>
2021-09-16 08:01:22 -0700

    Adding finished code

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,18 +13,34 @@ class Cats {
 class Kitten1 extends Cats {
     constructor(name, age, hairColor, collarColor) {
         super(name, age, hairColor);
-        this._collarColor = collarColor;
+        this.collarColor = collarColor;
     }
-
+    getCollar(kittenCollarColor) {
+        this.collarColor = kittenCollarColor;
+    }
 
 }
 
 class Kitten2 extends Cats {
     constructor(name, age, hairColor, collarColor) {
         super(name, age, hairColor, collarColor);
-        this._collarColor = collarColor;
         this.toys = [];
     }
-
+    getToys(toy) {
+        this.toys.push(toy);
+    }
     
 }
+const kitten1 = new Kitten2("joe",1,"black", "red");
+const kitten2 = new Kitten2("bob",2,"brown", "blue");
+
+kitten1.greet();
+
+
+console.log(kitten1);
+
+kitten2.greet();
+kitten2.getToys("Mouse");
+
+
+console.log(kitten2);
