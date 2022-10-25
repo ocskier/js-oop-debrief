528b78954888b44464fc1cb41f643d67cd0b95c5
javi <12883412+xaviif@users.noreply.github.com>
2022-10-25 13:43:48 -0400

    bold text

--- assets/js/Animals.js
+++ assets/js/Animals.js
@@ -13,12 +13,12 @@ class Animals{
 } 
 Animals.prototype.speech = function(){
   console.log(camelize(this.nickname))
-  if(this.soul === 1) return this.chat.message(this.nickname, `${this.nickname}: Hi, my nickname is ${this.nickname}! I'm a ${this.color} ${this.species}. I'm ${this.age} years old. I have a soul!`)
-  else return this.chat.message(this.nickname, `${this.nickname}: Hi, my nickname is ${this.nickname}! I was a ${this.color} ${this.species}. I was ${this.age} years old. I don't have a soul anymore. :(`)
+  if(this.soul === 1) return this.chat.message(this.nickname, `Hi, my nickname is ${this.nickname}! I'm a ${this.color} ${this.species}. I'm ${this.age} years old. I have a soul!`)
+  else return this.chat.message(this.nickname, ` Hi, my nickname is ${this.nickname}! I was a ${this.color} ${this.species}. I was ${this.age} years old. I don't have a soul anymore. :(`)
 }
 
 Animals.prototype.speak = function(){
-  return this.chat.message(this.nickname, `${this.nickname}: ${this.sound}`)
+  return this.chat.message(this.nickname, `${this.sound}`)
 }
 
 class Cat extends Animals{
@@ -56,7 +56,7 @@ Animals.prototype.takeDamage = function(dmg){
   //this.health = this.health - dmg
 }
 Animals.prototype.say = function(msg){
-  return this.chat.message(this.nickname, `${this.nickname}: ${msg}`)
+  return this.chat.message(this.nickname, `${msg}`)
 }
 Animals.prototype.fight = function(thing){
   thing.takeDamage(this.damage)
--- assets/js/Chat.js
+++ assets/js/Chat.js
@@ -3,17 +3,20 @@ class Chat{
     this.$ = $('#chatlog')
   }
 }
+Chat.prototype.message = function(whom, str, action){
+  let $chat = $(document.createElement("div"))
+  $chat.addClass('msg')
+  $chat.attr("whom", camelize(whom))
+  if(action==="action") {
+    $chat.addClass('action')
+    $chat.text(str)
+  }
+  else $chat.html(`<b>${whom}</b>: `+str)
+  this.$.append($chat);
+}
 function camelize(str) {
   return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
     if (+match === 0) return ""; 
     return index === 0 ? match.toLowerCase() : match.toUpperCase();
   });
-}
-Chat.prototype.message = function(whom, str, action){
-  let $chat = $(document.createElement("div"))
-  $chat.addClass('msg')
-  $chat.attr("whom", camelize(whom))
-  if(action==="action") $chat.addClass('action')
-  $chat.text(str)
-  this.$.append($chat);
 }
